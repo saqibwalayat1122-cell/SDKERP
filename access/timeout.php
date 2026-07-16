7d3e9d68dde642df37c648d8e38b6012db28ef20
saqibwalayat1122-cell <saqibwalayat1122@gmail.com>
2026-07-16 14:56:00 +0500

    new

--- access/timeout.php
+++ access/timeout.php
@@ -19,13 +19,15 @@ include_once($path_to_root . "/includes/session.inc");
 include($path_to_root .'/access/login.php');
 
 if (get_post('SubmitUser') && $_SESSION['wa_current_user']->logged_in()) {
-	// After successfull login repeat last ajax call.
-	// Login form consists all post variables from last ajax call.
-echo "<script>
+	// After successful login repeat last ajax call when opened as timeout popup.
+	// Otherwise redirect to the main application page.
+	echo "<script>
 	var o = opener;
 	if (o) {
 		o.JsHttpRequest.request(document.getElementsByName('SubmitUser')[0], o.document.forms[0]);
-		close();
+		window.close();
+	} else {
+		window.location = '$path_to_root/index.php';
 	}
 </script>";
 }
