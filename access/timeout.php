682d01641674d014a9c24faa88c0f356ae987392
saqibwalayat1122-cell <saqibwalayat1122@gmail.com>
2026-07-16 15:22:24 +0500

    dd

--- access/timeout.php
+++ access/timeout.php
@@ -16,12 +16,14 @@ $path_to_root = '..';
 $page_security = 'SA_OPEN';
 include_once($path_to_root . "/includes/session.inc");
 
+ob_start();
 include($path_to_root .'/access/login.php');
+$page = ob_get_clean();
 
 if (get_post('SubmitUser') && $_SESSION['wa_current_user']->logged_in()) {
 	// After successful login repeat last ajax call when opened as timeout popup.
 	// Otherwise redirect to the main application page.
-	echo "<script>
+	$script = "<script>
 	var o = opener;
 	try {
 		if (o && o.JsHttpRequest && typeof o.JsHttpRequest.request == 'function') {
@@ -36,6 +38,7 @@ if (get_post('SubmitUser') && $_SESSION['wa_current_user']->logged_in()) {
 	} catch (e) {
 		window.location = '$path_to_root/index.php';
 	}
-</script>";
-	echo "<noscript><center><p>Login successful. <a href='$path_to_root/index.php'>Click here to continue</a>.</p></center></noscript>";
+</script><noscript><center><p>Login successful. <a href='$path_to_root/index.php'>Click here to continue</a>.</p></center></noscript>";
+	$page = preg_replace('~</body>\s*</html>~i', $script.'</body></html>', $page, 1);
 }
+echo $page;
